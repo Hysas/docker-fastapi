8c1b698d16c2986af7ad515f905c722c13e23c0a
Hysas <ssstefannn@gmail.com>
2023-02-05 00:22:42 +0100

    removed debugger

--- src/main.py
+++ src/main.py
@@ -18,9 +18,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-import debugpy
-debugpy.listen(("0.0.0.0", 5678))
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router) 
