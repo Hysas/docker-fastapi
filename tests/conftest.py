0de76647ef79ff3d9aaa39eca891ebab0b31f2c4
Hysas <ssstefannn@gmail.com>
2023-02-11 22:00:31 +0100

    tset3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
-import requests
-from urllib.parse import urljoin
-from urllib3.util.retry import Retry
-from requests.adapters import HTTPAdapter
+# import requests
+# from urllib.parse import urljoin
+# from urllib3.util.retry import Retry
+# from requests.adapters import HTTPAdapter
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base   
@@ -11,7 +11,7 @@ from src import config, models, oauth2
 from src.main import app
 from src.database import get_db
 
-pytest_plugins = ["docker_compose"]
+# pytest_plugins = ["docker_compose"]
 
 
 NODE_ENV = config.settings.NODE_ENV
@@ -27,28 +27,28 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-@pytest.fixture(scope="function")
-def wait_for_api(function_scoped_container_getter):
-    """Wait for the api from my_api_service to become responsive"""
-    request_session = requests.Session()
-    retries = Retry(total=5,
-                    backoff_factor=0.1,
-                    status_forcelist=[500, 502, 503, 504])
-    request_session.mount('http://', HTTPAdapter(max_retries=retries))
+# @pytest.fixture(scope="function")
+# def wait_for_api(function_scoped_container_getter):
+#     """Wait for the api from my_api_service to become responsive"""
+#     request_session = requests.Session()
+#     retries = Retry(total=5,
+#                     backoff_factor=0.1,
+#                     status_forcelist=[500, 502, 503, 504])
+#     request_session.mount('http://', HTTPAdapter(max_retries=retries))
 
-    service = function_scoped_container_getter.get("my_api_service").network_info[0]
-    api_url = "http://%s:%s/" % (service.hostname, service.host_port)
-    assert request_session.get(api_url)
-    return request_session, api_url
+#     service = function_scoped_container_getter.get("my_api_service").network_info[0]
+#     api_url = "http://%s:%s/" % (service.hostname, service.host_port)
+#     assert request_session.get(api_url)
+#     return request_session, api_url
 
-def test_read_and_write(wait_for_api):
-    """The Api is now verified good to go and tests can interact with it"""
-    request_session, api_url = wait_for_api
-    data_string = 'some_data'
-    request_session.put('%sitems/2?data_string=%s' % (api_url, data_string))
-    item = request_session.get(urljoin(api_url, 'items/2')).json()
-    assert item['data'] == data_string
-    request_session.delete(urljoin(api_url, 'items/2'))
+# def test_read_and_write(wait_for_api):
+#     """The Api is now verified good to go and tests can interact with it"""
+#     request_session, api_url = wait_for_api
+#     data_string = 'some_data'
+#     request_session.put('%sitems/2?data_string=%s' % (api_url, data_string))
+#     item = request_session.get(urljoin(api_url, 'items/2')).json()
+#     assert item['data'] == data_string
+#     request_session.delete(urljoin(api_url, 'items/2'))
 
 @pytest.fixture
 def session():    
